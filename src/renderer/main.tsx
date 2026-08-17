d1dcd4b4c301b73bc21d8241fe762581bc00b368
brkeaslan5561 <berkeaslan.61@outlook.com>
2026-08-17 13:27:32 +0300

    Keep updater routing in preload bridge

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { AutomaticUpdateEnhancement } from "./AutomaticUpdateEnhancement";
 import { DesktopEnhancements } from "./DesktopEnhancements";
 import { DisplaySettingsEnhancement } from "./DisplaySettingsEnhancement";
 import { EncounterDeletionEnhancement } from "./EncounterDeletionEnhancement";
@@ -24,7 +23,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <>
         <App />
         <DesktopEnhancements />
-        <AutomaticUpdateEnhancement />
         <EncounterDeletionEnhancement />
         <DisplaySettingsEnhancement />
       </>
